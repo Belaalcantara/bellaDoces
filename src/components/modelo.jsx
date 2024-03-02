167d0d40471a3ad609c182809234c803e7d5574c
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2024-03-02 17:59:57 -0300

    criacao do componete da pagina que seria o menu de itens

--- src/components/modelo.jsx
+++ src/components/modelo.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { View, StyleSheet, Text, Image } from 'react-native'
 
-function categorias() {
+function Itens({titulo, img, frase }) {
   return (
-    <div>
-        
-    </div>
+    <View style={style.itens}>
+    <Text style={style.titulo}>{titulo}</Text>
+    <Image style={style.img}>{img}</Image>
+    <Text style={style.frase}>{frase}</Text>
+    </View>
   )
 }
 
-export default categorias
+
+const style = StyleSheet.create({
+ itens:{
+  backgroundColor: "black", 
+ }
+
+
+})
+
+
+
+export default Itens
