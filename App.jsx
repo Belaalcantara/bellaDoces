6881636f082bcce4046d61fc0075e9562c56ef96
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2024-03-02 18:03:57 -0300

    alteracoes na pagina app lincando as telas

--- App.jsx
+++ App.jsx
@@ -1,11 +1,18 @@
 
-import {  View } from 'react-native';
+
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Home from './src/screens/Home/index';
 import Itens from './src/screens/Itens/index';
+import Cafes from './src/screens/DetalheCafes';
+import Bolo from './src/screens/Bolos';
+import TortaS from './src/screens/TortaSalgada';
+import TortaD from './src/screens/TortaDoce';
+
+
+
 export default function App() {
   const Stack = createNativeStackNavigator()
   return (
@@ -15,8 +22,13 @@ export default function App() {
 
       <Stack.Navigator>
 
-        <Stack.Screen name="Seja bem vindo" component={Home}/>
-        <Stack.Screen name="itens" component={Itens}/>
+        <Stack.Screen name="BellaDoces" component={Home}/>
+        <Stack.Screen name="Produtos" component={Itens}/>
+        <Stack.Screen name="Café" component={Cafes}/>
+        <Stack.Screen name="Bolos" component={Bolo}/>
+        <Stack.Screen name="Torta salgada" component={TortaS}/>
+        <Stack.Screen name="Torta doce" component={TortaD}/>
+
 
       </Stack.Navigator>
 
